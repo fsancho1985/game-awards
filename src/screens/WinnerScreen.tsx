cb51898469e54f8353b0c76a276d0773d3792ea5
Francisco Sancho <frsancho1985@gmail.com>
2022-12-09 10:31:31 -0300

    Adding Screens

---
+++ src/screens/WinnerScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {View, Text} from 'react-native'
+
+export function WinnerScreen() {
+  return(
+    <View>
+      <Text>Winner Screen</Text>
+    </View>
+  )
+}
